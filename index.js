2c3e59b5eea96bbeeb1a914f1f69479b8bdc6c25
Anna Ivashkina <annapolushkina@gmail.com>
2022-06-20 19:15:23 -0400

    changed index.js

--- index.js
+++ index.js
@@ -33,11 +33,9 @@ app.get("/projects", function (req, res) {
     res.render("projects");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-  console.log("Server started succesfully");
+
+app.listen(port, () => {
+    console.log("Server started succesfully");
 });
